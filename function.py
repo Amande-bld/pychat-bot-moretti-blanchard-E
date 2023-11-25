78952a593462f974d88083f8c2c3a33504da262f
SangonomiyaMidona <150732459+SangonomiyaMidona@users.noreply.github.com>
2023-11-25 16:19:33 +0100

    Update function.py

--- function.py
+++ function.py
@@ -32,28 +32,6 @@ def display_list_president(dictionary_president):
     for name in dictionary_president.keys():
         print(name)
 
-def convert_file_lower_case(files_names,directory):
-    for file_name in files_names :
-        # Création chemin d'acces du fichier 
-        input_file_path = directory + '/' + file_name
-        # Ouverture fichier 
-        with open(input_file_path,'r') as content:
-            #Création chemin ou sera rangé le fichier modifier 
-            output_file_path = "./cleaned" + '/' + file_name + "copie.txt"
-            #Ouverture fichie copie
-            with open(output_file_path, 'a') as copy:
-                # modification des majusucles en miniscule 
-                line = content.readline()
-                while line != '':
-                    line_mod =''
-                    for car in line :
-                        if car >= 'A' and car <= 'Z':
-                            car = chr(ord(car)+ 32 )
-                        line_mod += car
-                    # Ligne transformer en minuscule réecrite dans la copie 
-                    copy.write(line_mod)
-                    ligne = content.readline()
-
 
 def replacement_punctuation_(files_names):
     for file_name in files_names:
