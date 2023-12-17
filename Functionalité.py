9c900cb9395d2cba0526bc2ad7eb0f49cd1863fe
Cathaleya-10 <150789482+Cathaleya-10@users.noreply.github.com>
2023-12-17 20:03:40 +0100

    modification

--- Functionalité.py
+++ Functionalité.py
@@ -1,48 +1,59 @@
 from function import *
 
+# définition des variables de bases
 directory = "./speeches"
 files_names = list_of_files(directory, "txt")
-tf_idf_matrix = TD_IDF(files_names)
+tf_idf_matrix = TD_IDF_transposed(files_names)
 
 if __name__ == '__main__':
     # Fonctionalité 1
 
-    print("Les mots les moins importants sont :", mots_non_importants(files_names))
+    print("Les mots les moins importants sont :", non_important_words(files_names))
 
     # Fonctionalité 2
 
-    print("Le(s) mots les plus importants sont :", most_important_word(tf_idf_matrix))
+    print("Le(s) mot(s) le(s) plus important(s) sont :", most_important_word(tf_idf_matrix))
 
     # Fonctionalité 3
-    president_last_name = "Chirac"
+
+    president = ['Chirac', 'Giscard dEstaing', 'Hollande', 'Macron', 'Mitterrand', 'Sarkozy']
+    correct = False
+    while not correct:
+        president_last_name = input(
+            "Veuillez choisir un nom de président parmis cette liste : Chirac, Giscard dEstaing, Hollande, Macron, Mitterrand, Sarkozy : ")
+
+        if president_last_name in president:
+            correct = True
+        else:
+            print(
+                "Le président que vous avez choisis ne fait pas partie de la liste. Veuillez en choisir un faisant partie de la liste")
+    print("Le président choisis est :", president_last_name)
+
     word_count = word_per_president(files_names, president_last_name)
-    word_count_trié = mots_trie(word_count, files_names)
-    most_repetated = mots_plus_repeter(word_count_trié)
-    print("Le(s) mot(s) le(s) plus répété(s) par le président Chirac hormis les mots non importants sont",
+    sort_word_count = sort_words(word_count, files_names)
+    most_repetated = words_more_repeat(sort_word_count)
+    print("Le(s) mot(s) le(s) plus répété(s) par le président", president_last_name,
+          " hormis les mots non importants sont : ",
           most_repetated)
 
     # Fonctionalité 4
 
-    target_word = "nation"
-
+    target_word = input("Veuillez saisir le mot que vous souhiatez chercher : ")
+    target_word = target_word.lower()
+    # si on met un espace en plus lors de la saisie du mot recherché out of range
     mention = president_to_mention_topic(files_names, target_word)
-    print("Les présidents qui ont parlé de nation sont :", mention)
+    print("Les présidents qui ont parlé de nation sont :", list(mention.keys()))
 
     for president_last_name in mention:
         word_count[president_last_name] = word_per_president(files_names, president_last_name)
     most_repeated_president = list(president_speak_about_the_most_topic(files_names, target_word, mention))
 
-    print("Le président qui a répéter le plus de fois le mot", target_word, 'est', most_repeated_president[0])
+    print("Le président qui a répéter le plus de fois le mot", target_word, 'est le président',
+          most_repeated_president[0])
 
     # Fonctionalité 5
 
     target_words = ['climat', 'écologie']
     first_mention = first_president_to_mention_topic(files_names, target_words)
-    print("le premier président a parler de climat d'écologie dans l'ordre des fichiers est", first_mention)
-
-    # Fonctionilaté 6
-
-    non_important_words = mots_non_importants(files_names)
-    common_words = common_important_words_across_presidents(files_names, non_important_words)
-
-    print("Mots évoqués par tous les présidents (hormis les mots non importants) :", common_words)
+    first_mention = list(first_mention.keys())
+    print("Le premier président a parler de climat ou/et d'écologie est le président ", first_mention[0])
